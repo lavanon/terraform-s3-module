efb43856aa01624df423069d994425b6574f1a7c
codellah <40672446+codellah@users.noreply.github.com>
2025-09-04 09:42:49 +0100

    Simplest Unit of work.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.this.id
-}
-
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.this.arn
+output "success" {
+  description = "Confirmation that the module ran"
+  value       = "It worked for agent ${var.agent}"
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,4 @@
-variable "region" {
-  description = "AWS region to create resources in"
+variable "agent" {
+  description = "Identifier for the Quali/Terraform agent"
   type        = string
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
-variable "acl" {
-  description = "Canned ACL for the S3 bucket"
-  type        = string
-  default     = "private"
-}
-
-variable "tags" {
-  description = "Tags to apply to the bucket"
-  type        = map(string)
-  default     = {}
 }
